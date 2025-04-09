1d0c27bd909191f577e961ff7c4f70631f8fef36
Cody <nytemairqt@gmail.com>
2025-04-10 02:05:10 +1000

    Added filter response curves

--- Scripts/ScriptProcessors/Unspeakable/Interface.js
+++ Scripts/ScriptProcessors/Unspeakable/Interface.js
@@ -1,11 +1,35 @@
-Content.makeFrontInterface(700, 100);
+Content.makeFrontInterface(700, 160);
 
-const knbControls = [Content.getComponent("knbInputGain"), Content.getComponent("knbOutputGain"), Content.getComponent("knbMode"), Content.getComponent("knbClarity")];
+// Declare Const
+const knb = [Content.getComponent("knbInputGain"), Content.getComponent("knbOutputGain"), Content.getComponent("knbMode"), Content.getComponent("knbClarity")];
+const btn = [Content.getComponent("btnEQOff"), Content.getComponent("btnEQA"), Content.getComponent("btnEQB"), Content.getComponent("btnEQC")];
 const scriptFX = Synth.getEffect("scriptFX");
+const eq = [Synth.getEffect("eqA"), Synth.getEffect("eqB"), Synth.getEffect("eqC")];
 
+// Functionality
 
-const eq = [Synth.getEffect("eqA"), Synth.getEffect("eqB"), Synth.getEffect("eqC"), Synth.getEffect("eqD")];
+inline function onknbControl(component, value)
+{
+	switch (component)
+	{
+		case knb[0]: // Input Gain
+			scriptFX.setAttribute(scriptFX.inputGain, value);
+			break;
+		case knb[1]: // Output Gain
+			scriptFX.setAttribute(scriptFX.outputGain, value);
+			break;
+		case knb[2]: // Mode
+			scriptFX.setAttribute(scriptFX.mode, value);			
+			break;
+		case knb[3]: // Clarity
+			scriptFX.setAttribute(scriptFX.clarity, value);
+			break;
+	}
+}
 
+for (k in knb)
+	k.setControlCallback(onknbControl);
+	
 inline function toggleEQ(index)
 {
 	for (i=0; i<eq.length; i++)
@@ -13,29 +37,30 @@ inline function toggleEQ(index)
 			eq[i].setBypassed(1);
 	eq[index].setBypassed(0);
 }
-
-inline function onknbControl(component, value)
+	
+inline function onbtnControl(component, value)
 {
-	switch (component)
-	{
-		case knbControls[0]: // Input Gain
-			scriptFX.setAttribute(scriptFX.inputGain, value);
-			break;
-		case knbControls[1]: // Output Gain
-			scriptFX.setAttribute(scriptFX.outputGain, value);
-			break;
-		case knbControls[2]: // Mode
-			toggleEQ(value);
-			scriptFX.setAttribute(scriptFX.mode, value);			
-			break;
-		case knbControls[3]: // Clarity
-			scriptFX.setAttribute(scriptFX.clarity, value);
-			break;
-	}
+	if (value)
+		switch (component)
+		{
+			case btn[0]: // Bypass
+				for (e in eq)
+					e.setBypassed(1);
+				break;
+			case btn[1]: // A
+				toggleEQ(0);
+				break;
+			case btn[2]: // B
+				toggleEQ(1);
+				break;
+			case btn[3]: // C
+				toggleEQ(2);
+				break;
+		}
 }
 
-for (k in knbControls)
-	k.setControlCallback(onknbControl);function onNoteOn()
+for (b in btn)
+	b.setControlCallback(onbtnControl);function onNoteOn()
 {
 	
 }
